feae2439f622df311532c3c634efd14444bd9be0
Abdessamad404 <abdessamadzenasni@gmail.com>
2026-01-29 12:12:42 +0100

    daily challenge 1 done

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,14 @@
-// ====== Daily Challenge 1
+//? ====== Daily challenge: Not Bad
+//* Write a JavaScript function that takes a string as input and checks if the word "not" appears before the word "bad". If "not" comes before "bad", replace the whole"not...bad" substring with "good" and return the modified string.
+//* If "not" does not come before "bad", return the original string.
+function notBad(str) {
+  const notIndex = str.indexOf("not");
+  const badIndex = str.indexOf("bad");
+  if (notIndex !== -1 && badIndex !== -1 && notIndex < badIndex) {
+    return str.slice(0, notIndex) + "good" + str.slice(badIndex + 3);
+  } else {
+    return str;
+  }
+}
 
-// ====== Daily Challenge 2
-
-// ====== Daily Challenge 3
+console.log(notBad("This movie is not so bad")); // Output: "This movie is good"
