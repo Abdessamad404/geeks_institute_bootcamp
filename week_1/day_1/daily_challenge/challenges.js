1e145a4dae05b3f98eb6fc0bbd1c9135804404d5
Abdessamad404 <abdessamadzenasni@gmail.com>
2026-01-29 12:16:35 +0100

    challenge 2 done

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -11,4 +11,26 @@ function notBad(str) {
   }
 }
 
-console.log(notBad("This movie is not so bad")); // Output: "This movie is good"
+// // console.log(notBad("This movie is not so bad")); // Output: "This movie is good"
+
+//? Daily challenge: Stars
+//* Write a JavaScript function that takes a positive integer n as input and prints a right-angled triangle of stars with n rows.
+// method 1
+function printStars(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log("*".repeat(i));
+  }
+}
+// // printStars(5);
+
+// method 2 : nested loops
+function printStarsAlternative(n) {
+  for (let i = 1; i <= n; i++) {
+    let stars = "";
+    for (let j = 1; j <= i; j++) {
+      stars += "*";
+    }
+    console.log(stars);
+  }
+}
+// // printStarsAlternative(10);
