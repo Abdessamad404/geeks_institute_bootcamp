bb3cdad9769cb5e5c1a6de330590f02a8221e121
Abdessamad404 <abdessamadzenasni@gmail.com>
2026-02-03 13:17:39 +0100

    Day 3: Daily Challenge: Tell the story done

--- week_1/day_3/daily_challenges/challenges.js
+++ week_1/day_3/daily_challenges/challenges.js
@@ -16,16 +16,16 @@ form.addEventListener("submit", function (event) {
     }
     values.push(inputs[i].value);
   }
+
   //? Create three different stories using the values from the inputs
   // values[0] = noun, values[1] = adjective, values[2] = person, values[3] = verb, values[4] = place
   const stories = [
     `Once upon a time in ${values[4]}, there lived a ${values[1]} ${values[0]} who loved to ${values[3]}. Every day, ${values[2]} would ${values[3]} and dream of visiting that special place. One day, everything changed when a mysterious visitor appeared, leading to an unforgettable adventure!`,
-    `In ${values[4]}, a ${values[1]} ${values[0]} named ${values[2]} decided to ${values[3]} for the very first time. The ${values[1]} creature traveled far and wide, eventually discovering that ${values[3]} was their true passion!`,
+    `In ${values[4]}, a ${values[1]} ${values[0]} named ${values[2]} decided to ${values[3]} for the very first time. The ${values[1]} creature traveled far and wide, eventually discovering that ${values[3]}ing was their true passion!`,
     `${values[2]} was a ${values[1]} person who loved a peculiar ${values[0]}. Every day, ${values[2]} would ${values[3]} in ${values[4]}, becoming famous throughout the land for their dedication to ${values[3]}!`,
   ];
 
   const storyDiv = document.getElementById("story");
-  currentStoryIndex = 0;
   storyDiv.textContent = stories[currentStoryIndex];
 
   //? Add a shuffle button only if it doesn't already exist
@@ -35,11 +35,11 @@ form.addEventListener("submit", function (event) {
     shuffleButton.style.color = "red";
     shuffleButton.style.marginLeft = "10px";
     storyDiv.insertAdjacentElement("afterend", shuffleButton);
-
-    //? Shuffle the stories when the button is clicked
-    shuffleButton.addEventListener("click", function () {
-      currentStoryIndex = (currentStoryIndex + 1) % stories.length;
-      storyDiv.textContent = stories[currentStoryIndex];
-    });
   }
+
+  //? Shuffle the stories when the button is clicked
+  shuffleButton.addEventListener("click", function () {
+    currentStoryIndex = (currentStoryIndex + 1) % stories.length;
+    storyDiv.textContent = stories[currentStoryIndex];
+  });
 });
