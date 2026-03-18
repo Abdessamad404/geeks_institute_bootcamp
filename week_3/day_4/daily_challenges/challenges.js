11319f54c7b288fa45d7216faff1e3f7271f5a10
Abdessamad404 <abdessamadzenasni@gmail.com>
2026-03-18 12:53:16 +0000

    feat: add week 3 day 4 Daily Challenge

--- week_3/day_4/daily_challenges/challenges.js
+++ week_3/day_4/daily_challenges/challenges.js
@@ -1,4 +1,7 @@
-// Function 1: makeAllCaps
+// // const prompt = require("prompt-sync")();
+
+//! 1st daily challenge
+//* Function 1: makeAllCaps
 function makeAllCaps(words) {
   return new Promise((resolve, reject) => {
     const allStrings = words.every(word => typeof word === "string");
@@ -12,7 +15,7 @@ function makeAllCaps(words) {
   });
 }
 
-// Function 2: sortWords
+//* Function 2: sortWords
 function sortWords(words) {
   return new Promise((resolve, reject) => {
     if (words.length > 4) {
@@ -42,3 +45,110 @@ makeAllCaps(wordsArray)
 // Output:
 // Uppercased Words: [ 'ELDERBERRY', 'APPLE', 'BANANA', 'CHERRY', 'DATE' ]
 // Sorted Words: [ 'APPLE', 'BANANA', 'CHERRY', 'DATE', 'ELDERBERRY' ]
+
+//! 2nd daily challenge
+const morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`;
+
+//* Create three functions. The two first functions should return a promise..
+function toJs(morseCode) {
+  return new Promise((resolve, reject) => {
+    try {
+      const morseObj = JSON.parse(morseCode);
+      resolve(morseObj);
+    } catch (error) {
+      reject("Error: Invalid JSON string.");
+    }
+  });
+}
+
+const toMorse = morseJS => {
+  return new Promise((resolve, reject) => {
+    const userInput = prompt("Enter a word or sentence:").toLowerCase();
+
+    const translation = [];
+
+    for (let char of userInput) {
+      if (char === " ") continue; // optional: skip spaces
+
+      if (!morseJS[char]) {
+        reject(`Character "${char}" doesn't exist in Morse`);
+        return;
+      }
+
+      translation.push(morseJS[char]);
+    }
+
+    resolve(translation);
+  });
+};
+
+const joinWords = morseTranslation => {
+  const output = morseTranslation.join("\n");
+
+  const outputTag = document.getElementById("output");
+
+  outputTag.textContent = output;
+
+  document.body.appendChild(outputTag);
+};
+
+toJs(morse)
+  .then(toMorse)
+  .then(joinWords)
+  .catch(error => {
+    console.error(error);
+  });
+
+// Output:
+// Enter a word or sentence: hello
+// .-...
+// .-...
+// .-...
+// .-...
+// .-...
